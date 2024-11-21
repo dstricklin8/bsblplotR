71c73a802c87b31b5af14b204ee7165c3741f908
Donald Stricklin <DonaldStricklin2023@u.northwestern.edu>
2024-11-21 12:29:34 -0600

    Creating First Function

--- R/plot_pitch_locations.R
+++ R/plot_pitch_locations.R
@@ -1,17 +1,29 @@
-#' A Cat Function
+#' @title **Plot Pitch Locations**
 #'
-#' This function allows you to express your love of cats.
-#' @param love Do you love cats? Defaults to TRUE.
-#' @keywords cats
+#' @description This function allows you to plot all pitch locations from the dataset
+#' @param input_df The file you want to use to plot the pitch locations.
+#' @importFrom ggplot2 ggplot geom_point geom_curve geom_segment coord_fixed theme stat_density2d xlim ylim scale_fill_continuous aes_string element_text scale_fill_manual aes
 #' @export
 #' @examples
-#' cat_function()
+#' plot_pitch_location()
 
-cat_function <- function(love=TRUE){
-  if(love==TRUE){
-    print("I love cats!")
-  }
-  else {
-    print("I am not a cool person.")
-  }
+plot_pitch_location <- function(input_df) {
+
+  pitch_locations_plot <- input_df %>%
+    ggplot() +
+    geom_polygon(home_plate, mapping = aes(x/12, z/12), fill = "#ededed", color = "lightgrey") +
+    geom_point(mapping = aes(x = PlateLocSide, y = PlateLocHeight,
+                             fill = TaggedPitchType), shape = 21, size = 5, show.legend = F) +
+    geom_path(sz, mapping = aes(x/12, z/12)) +
+    theme_minimal() +
+    scale_fill_manual(values = pitch_colors) +
+    scale_color_manual(values = pitch_colors) +
+    coord_equal(xlim = c(-2, 2), ylim = c(0, 5)) +
+    labs(x = "", y = "", fill = "") +
+    theme(panel.grid = element_blank(),
+          axis.text = element_blank())
+
+  return(pitch_locations_plot)
+
+
 }
